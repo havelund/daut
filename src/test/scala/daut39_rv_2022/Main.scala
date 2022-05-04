a82dfd7bdbee9c4e5c8c12557146efc67f76086d
khavelun <klaus.havelund@jpl.nasa.gov>
2022-05-04 15:34:56 -0700

    updated lock acquisition example for RV 2022

--- src/test/scala/daut39_rv_2022/Main.scala
+++ src/test/scala/daut39_rv_2022/Main.scala
@@ -3,60 +3,79 @@ package daut39_rv_2022
 import daut._
 
 /**
- * A thread acquiring a lock must eventually release it, and while acquired
- * the lock cannot be acquired by another thread before being released again.
+  * A thread acquiring a lock must eventually release it, with no further
+  * acquisitions in between, and thread can only release a lock if it has acquired
+  * it, and not yet released it (for each lock we want acquisitions and releases
+  * to alternate).
  */
 
 /*
-Pyton Monitor:
+===============
+Python version:
+===============
 
-class M1(Monitor):
-    def transition(self, event):
+class M3(Monitor):
+    def key(self, event) -> Optional[object]:
         match event:
-            case Acquire(thread, lock):
-                return M1.DoRelease(thread, lock)
+            case Acquire(_, lock) | Release(_, lock):
+                return lock
+
+    @initial
+    class Idle(NextState):
+        def transition(self, event):
+            match event:
+                case Acquire(thread, lock):
+                    return M3.DoRelease(thread, lock)
 
     @data
-    class DoRelease(HotState):
+    class DoRelease(HotNextState):
         thread: str
         lock: int
 
         def transition(self, event):
             match event:
-                case Acquire(thread, self.lock)
-                  if thread != self.thread:
-                    return error()
                 case Release(self.thread, self.lock):
-                    return ok
+                    return M3.Idle()
  */
 
 trait Event
 case class Acquire(thread: String, lock: Int) extends Event
 case class Release(thread: String, lock: Int) extends Event
 
-class M1 extends Monitor[Event] {
-  always {
-    case Acquire(thread, lock) => DoRelease(thread, lock)
+class M3 extends Monitor[Event] {
+  override def keyOf(event: Event): Option[Int] = {
+    event match {
+      case Acquire(_, lock) => Some(lock)
+      case Release(_, lock) => Some(lock)
+    }
+  }
+
+  case class Idle() extends state {
+    wnext {
+      case Acquire(thread, lock) => DoRelease(thread, lock)
+    }
   }
 
   case class DoRelease(thread: String, lock: Int) extends state {
-    hot {
-      case Acquire(thr, `lock`)  if thr != thread => error
-      case Release(`thread`, `lock`) => ok
+    next {
+      case Release(`thread`, `lock`) => Idle()
     }
   }
+
+  initial(Idle())
 }
 
 object Main {
   def main(args: Array[String]) {
-    val m = new M1
+    val m = new M3
+    DautOptions.DEBUG = false
     val trace =  List(
       Acquire("T1", 1),
-      Acquire("T1", 1),  // double acquisition by same thread: ok
-      Release("T1", 1),
       Acquire("T2", 2),
+      Acquire("T3", 3),
+      Release("T1", 1),
       Acquire("T3", 2),  // double acquisition by other thread: bad
-      // missing release of lock 2 by T3: bad
+      // missing release of lock 3 by T3: bad
     )
     m.verify(trace)
   }
