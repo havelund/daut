72df5426fd36fdce7f9b06b8d3892420486e4c39
Klaus Havelund <klaus.havelund@jpl.nasa.gov>
2024-05-23 16:49:51 -0700

    Update

---
+++ src/test/scala/daut41_mexec/Main.scala
@@ -0,0 +1,77 @@
+package daut41_mexec
+
+
+import daut.{Monitor}
+
+enum MEXECmd:
+  case START
+  case CLEANUP
+  case STOP
+
+type Time = Int
+
+// Abstract events:
+
+trait AbstractEvent
+case class Command(taskId: Int, cmdNum: Int) extends AbstractEvent
+
+// Concrete events:
+
+trait MexecEvent
+case class DispatchRequest(taskId: Int, cmdNum: Int, cmdType: MEXECmd) extends MexecEvent
+case class DispatchReply(success: Boolean, valStatus: Int, taskId: Int, cmdType: MEXECmd) extends MexecEvent
+case class CommandComplete(opCode: Int, cmplStatus: Boolean, cmdLen: Int, dispatchTime: Time,
+                           cmdNum: Int, cmdType: MEXECmd, meta1: Int, meta2: Int) extends MexecEvent
+
+// Abstract monitor:
+
+class AbstractMonitor extends Monitor[AbstractEvent] {
+  always {
+    case Command(taskId1, cmdNum1) => always {
+      case Command(taskId2, cmdNum2) =>
+        println(s"two commands: $taskId1 $cmdNum1 -> $taskId2 $cmdNum2")
+        ensure(taskId1 != taskId2 || cmdNum1 != cmdNum2)
+    }
+  }
+}
+
+class ConcreteMonitor extends Monitor[MexecEvent] {
+  val abstractMonitor = AbstractMonitor()
+
+  always {
+    case DispatchRequest(taskId, cmdNum, MEXECmd.START) =>
+      hot {
+        case DispatchRequest(`taskId`, _, _) => error
+        case DispatchReply(true, valStatus, `taskId`, MEXECmd.START) =>
+          hot {
+            case CommandComplete(opCode, cmplStatus, cmdLen, dispatchTime, `cmdNum`, MEXECmd.START, meta1, meta2) =>
+              abstractMonitor(Command(taskId, cmdNum))
+              println(s"$taskId $cmdNum $valStatus $opCode $cmplStatus $cmdLen $dispatchTime $meta1 $meta2")
+          }
+      }
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val trace: List[MexecEvent] = List(
+      DispatchRequest(1, 2, MEXECmd.START),
+      DispatchReply(true, 10, 1, MEXECmd.START),
+      CommandComplete(42, false, 20, 2030, 2, MEXECmd.START, 100, 200),
+
+      DispatchRequest(3, 4, MEXECmd.START),
+      DispatchReply(true, 10, 3, MEXECmd.START),
+      CommandComplete(42, true, 20, 2032, 4, MEXECmd.START, 100, 200),
+
+      DispatchRequest(1, 2, MEXECmd.START),
+      DispatchReply(true, 10, 1, MEXECmd.START),
+      CommandComplete(42, false, 20, 2030, 2, MEXECmd.START, 100, 200)
+    )
+
+    val monitor = new ConcreteMonitor
+    monitor(trace)
+  }
+}
+
+
+
